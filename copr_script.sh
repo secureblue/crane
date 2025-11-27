776d9f21d7e3828269ce0234c39e309d919ba017
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-11-26 16:24:40 -0800

    dynamically update vendor tarball

---
+++ copr_script.sh
@@ -0,0 +1,25 @@
+#! /bin/bash -x
+
+# Copyright 2025 The Trivalent Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+set -oue pipefail
+
+cd crane
+VERSION=$(grep -E '^Version:' crane.spec | awk '{print $2}')
+mkdir generate_vendor
+cd generate_vendor
+go2rpm --name crane --profile vendor --version "${VERSION}" -s pkg/crane https://github.com/google/go-containerregistry/
+mv "go-containerregistry-${VERSION}-vendor.tar.bz2" ..
+cd ..
+rm generate_vendor -rf
+cd ..
