a628639a778446da7cc0b1bef18b7cef3f31d382
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-11-26 18:34:05 -0800

    fix: authors name

--- copr_script.sh
+++ copr_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -x
 
-# Copyright 2025 The Trivalent Authors
+# Copyright 2025 The Secureblue Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
