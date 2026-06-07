7cb3d59cbbbf3b7bb0665a8aa89cfa0cd49b688c
Daniel Hast <hast.daniel@protonmail.com>
2026-06-07 14:41:25 -0400

    fix: make %autochangelog work
    Manually apply `rpmautospec` to make `%autochangelog` work in Copr. Also
    make Copr script self-contained, and use subshells to make it easier to
    see which parts of the script are run from a subdirectory.

--- copr_script.sh
+++ copr_script.sh
@@ -1,26 +1,26 @@
-#! /bin/bash -x
+#!/bin/sh
 
-# Copyright 2025 The Secureblue Authors
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
-set -oue pipefail
+# Package dependencies for this script:
+# git-core go2rpm go-vendor-tools python3-specfile rpmautospec rpmdevtools
 
-cd crane
-VERSION=$(grep -E '^Version:' crane.spec | awk '{print $2}')
+set -eux
+
+git clone https://github.com/secureblue/crane.git
+VERSION=$(grep -oP -m1 '^Version:\s+\K\S+' crane/crane.spec)
+(
+    cd crane
+    cp go-vendor-tools.toml ..
+    rpmautospec process-distgit ./crane.spec ../crane.spec
+)
 mkdir -p generate_vendor
 cp go-vendor-tools.toml generate_vendor
-cd generate_vendor
-go2rpm --name crane --profile vendor --version "${VERSION}" -s pkg/crane https://github.com/google/go-containerregistry/
-mv "go-containerregistry-${VERSION}-vendor.tar.bz2" ..
-cd ..
-rm generate_vendor -rf
-mv ./* ../
+(
+    cd generate_vendor
+    go2rpm --name crane --profile vendor --version "${VERSION}" -s pkg/crane https://github.com/google/go-containerregistry/
+    mv "go-containerregistry-${VERSION}.tar.gz" "go-containerregistry-${VERSION}-vendor.tar.bz2" ..
+)
+rm -rf crane generate_vendor
